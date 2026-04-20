6c0b2851de91e54f43d49b508fae355b50ec6bb9
Aaditya Barnwal <barnwalaaditya5@gmail.com>
2026-04-20 11:41:14 +0530

    Updated

--- a.cpp
+++ a.cpp
@@ -2,33 +2,30 @@
 // #include <bits/stdc++.h>
 // using namespace std;
 
-// int main()
-// {
+// int main() {
 //     int n;
 //     cin >> n;
 
-//     vector<int> arr(n + 1);
-//     for (int i = 0; i <= n; i++)
-//         cin >> arr[i];
+//     vector<int> arr(n);
+//     for (int i = 0; i < n; i++) cin >> arr[i];
 
 //     vector<vector<int>> dp(n, vector<int>(n, 0));
 
-//     for (int len = 2; len <= n; len++)
-//     {
-//         for (int i = 0; i <= n - len; i++)
-//         {
+//     for (int len = 2; len < n; len++) {
+//         for (int i = 1; i < n - len + 1; i++) {
 //             int j = i + len - 1;
 //             dp[i][j] = INT_MAX;
 
-//             for (int k = i; k < j; k++)
-//             {
-//                 int cost = dp[i][k] + dp[k + 1][j] + arr[i] * arr[k + 1] * arr[j + 1];
+//             for (int k = i; k < j; k++) {
+//                 int cost = dp[i][k] + dp[k + 1][j]
+//                          + arr[i - 1] * arr[k] * arr[j];
 //                 dp[i][j] = min(dp[i][j], cost);
 //             }
 //         }
 //     }
 
-//     cout << dp[0][n - 1];
+//     cout << dp[1][n - 1];
+//     return 0;
 // }
 
 // /*
